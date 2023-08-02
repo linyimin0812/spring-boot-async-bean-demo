bf4eeab624952167b5ab24ab9593c673b27db069
linyimin <linyimin520812@gmail.com>
2023-08-02 19:13:24 +0800

    update health check url

--- src/main/java/io/github/linyimin0812/controller/MainController.java
+++ src/main/java/io/github/linyimin0812/controller/MainController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MainController {
 
-    @RequestMapping(path = "/", method = RequestMethod.GET)
+    @RequestMapping(path = "/actuator/health", method = RequestMethod.GET)
     public String index() {
         return "Hello World";
     }
